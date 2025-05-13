d58d208eaac9f9c1f214de81bb00292b27d56b83
Edily Mora <edilyrosa@gmail.com>
2025-05-12 20:43:31 -0400

    test CRUD

--- index copy.js
+++ index copy.js
@@ -24,36 +24,13 @@ app.get('/usuarios', async (req, res) => {//TODO
     }
   
     res.json(data) // Envía los usuarios como JSON
-  })
+})
 
 
-  app.post('/usuarios', async (req, res) => { //NEVESARIA PARA HACER PUT PQ FRONT VALIDA SU EXISTENCIA
-    const usuario = req.body; // { nombre, edad, profesion }
- 
-    if (!usuario.nombre || !usuario.edad ||  !usuario.email || !usuario.foto || !usuario.aceptacion || !usuario.genero){
-      return res.status(400).json({ error: 'Faltan datos obligatorios' })
-      console.log('faltan datos obligatorios')
-    }
-
+app.delete('/usuarios/:id', async (req, res) => {
+    const id = parseInt(req.params.id);//!Capturamos id q fue enviado como parametro dinamico. 
   
-    const { data, error } = await supabase
-      .from('usuarios')
-      .insert([{ ...usuario }])
-      .select();
-  
-    if (error) {
-      console.error('Error al crear usuario:', error);
-      return res.status(500).json({ error: 'Error al crear usuario' });
-    }
-  
-    res.status(201).json(data[0]);
-  });
-
-
-  app.delete('/usuarios/:id', async (req, res) => {
-    const id = parseInt(req.params.id);
-  
-    const { data, error } = await supabase
+    const { data, error } = await supabase //! .select() después de .delete() devuelve los registros eliminados en "data"
       .from('usuarios')
       .delete()
       .eq('id', id)
@@ -64,38 +41,59 @@ app.get('/usuarios', async (req, res) => {//TODO
       return res.status(500).json({ error: 'Error al eliminar usuario' });
     }
   
-    if (data.length === 0) {
+    if (data.length === 0) { //!Confirmar la eliminacion, data trae el ele eliminado, false y no entra
       return res.status(404).json({ error: 'Usuario no encontrado' });
     }
   
     res.status(204).send(); // Eliminación exitosa, sin contenido
-  });
-  
+});
 
-  app.get('/usuarios/:id', async (req, res) => {
+app.get('/usuarios/:id', async (req, res) => {//?NEVESARIA PARA HACER PUT PQ FRONT VALIDA SU EXISTENCIA ANTES
     const id = parseInt(req.params.id);
     const { data, error } = await supabase
       .from('usuarios')
       .select('*')
       .eq('id', id)
-      .single();
+      .single(); //!.single() para q la consulta retorne un registro TDD Objeto en lugar de un arry con 1 ele
   
     if (error) {
       return res.status(500).json({ error: 'Error al obtener usuario' });
     }
-    if (!data) {
+    if (!data) {//!si la consulta retorna folsy no fue exitosa. 
       return res.status(404).json({ error: 'Usuario no encontrado' });
     }
     res.json(data);
   });
+
+
+  app.post('/usuarios', async (req, res) => {
+    const usuario = req.body; //! 1. traemos el cuerpo de la peticion (form) { nombre, edad, profesion }
+ 
+    //!2. validamos que esten todos los campos, evitando data corrupta.
+    if (!usuario.nombre || !usuario.edad ||  !usuario.email || !usuario.foto || !usuario.aceptacion || !usuario.genero){
+      return res.status(400).json({ error: 'Faltan datos obligatorios' })
+      console.log('faltan datos obligatorios')
+    }
+
+    const { data, error } = await supabase
+      .from('usuarios')
+      .insert([{ ...usuario }])//! 3 .insert() espera un arry de Objs, incluso si se inserte un solo registro.
+      .select();
   
+    if (error) {
+      console.error('Error al crear usuario:', error);
+      return res.status(500).json({ error: 'Error al crear usuario' });
+    }
+  
+    res.status(201).json(data[0]); //!pasamos el registro creado
+  });
+
 
   app.put('/usuarios/:id', async (req, res) => {
-    const id = parseInt(req.params.id);
-    const usuario = req.body;
+    const id = parseInt(req.params.id); //! id para saber a quien voy a UPDATE
+    const usuario = req.body; //!Data a UPDATE
   
-    // Validar que al menos un campo venga para actualizar
-    if (
+    if ( // Validar que al menos un campo venga para actualizarm NO TODOS PUEDEN DAR TRUE
       !usuario.nombre &&
       !usuario.edad &&
       !usuario.email &&
@@ -106,7 +104,7 @@ app.get('/usuarios', async (req, res) => {//TODO
       return res.status(400).json({ error: 'Debes enviar al menos un campo para actualizar' });
     }
   
-    // Construir objeto con solo los campos definidos para actualizar
+    //! Construir objeto con la UPDATE data enviada desde front
     const camposActualizar = {};
     if (usuario.nombre) camposActualizar.nombre = usuario.nombre;
     if (usuario.edad) camposActualizar.edad = usuario.edad;
@@ -117,9 +115,9 @@ app.get('/usuarios', async (req, res) => {//TODO
   
     const { data, error } = await supabase
       .from('usuarios')
-      .update(camposActualizar)
+      .update(camposActualizar) //!simplemente pasamos el Obj
       .eq('id', id)
-      .select();
+      .select();//!obtenemos el ele update
   
     if (error) {
       console.error('Error al actualizar usuario:', error);
@@ -130,11 +128,9 @@ app.get('/usuarios', async (req, res) => {//TODO
       return res.status(404).json({ error: 'Usuario no encontrado' });
     }
   
-    res.json(data[0]); // Usuario actualizado
+    res.json(data[0]); //! enviamos Usuario actualizado
   });
   
-
-
 
 app.listen(PORT, ()=>{
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
